e1f013eaf2f955a60d0376fac60ce51f7ecea286
Manish Kumar <mankan2105@gmail.com>
2025-07-22 08:45:52 +0530

    this is cpp template

---
+++ cpp_template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+void setupIO() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+int main() { setupIO();
+    
+     cout <<"manish";
+    return 0;
+}
