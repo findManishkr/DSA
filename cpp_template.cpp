52d745e098cb0f49e2d3bd8a96669098ebecebc2
Manish Kumar <mankan2105@gmail.com>
2025-07-31 15:35:27 +0530

    this is the template

--- cpp_template.cpp
+++ cpp_template.cpp
@@ -3,8 +3,8 @@ using namespace std;
 void setupIO() {
     ios::sync_with_stdio(false); cin.tie(nullptr);
     #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
+        freopen("../input.txt", "r", stdin);
+        freopen("../output.txt", "w", stdout);
     #endif
 }
 
