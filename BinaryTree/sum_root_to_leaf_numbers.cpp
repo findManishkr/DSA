fe3b07011b2a6e0bf7c372a06f7be4e02582bad0
Manish Kumar <mankan2105@gmail.com>
2025-10-30 15:48:58 +0530

    draw tree diagram and question solved

---
+++ BinaryTree/sum_root_to_leaf_numbers.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define loop(i,a,b)     for(int i=(a); i<(b); i++)
+#define all(x)          (x).begin(), (x).end()
+
+void setupIO() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void f( TreeNode* root, string &s, vector<string>&ans ){
+              if( !root ) return ;
+
+              s += to_string( root->val);
+
+              if( !root->left && !root->right ){
+                   ans.push_back(s);
+              } 
+
+              f(root->left, s,  ans);
+              f(root->right, s, ans);
+
+             s.pop_back();         // backtracking step
+
+
+
+    }
+    int sumNumbers(TreeNode* root) {
+        
+           string res = "";
+           vector< string > ans;
+
+          f(root, res, ans);
+
+          int sum = 0;
+
+          for( string &str : ans)
+            sum += stoi( str);
+            
+
+          return sum;
+    }
+};
+int main(int argc, char const *argv[]) { setupIO();
+
+
+
+    return 0;
+}
