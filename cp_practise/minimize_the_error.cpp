f80f22f3411e9a5aa6bbb74c611e65c6a6535af2
Manish Kumar <mankan2105@gmail.com>
2025-11-05 22:25:43 +0530

    .its 10:25 pm

---
+++ cp_practise/minimize_the_error.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define loop(i,a,b)     for(int i=(a); i<(b); i++)
+#define all(x)          (x).begin(), (x).end()
+
+
+int main(int argc, char const *argv[]) {  // setupIO();
+   int n, k1, k2;
+   cin >> n >> k1 >> k2;
+
+      ve
+
+     
+
+    return 0;
+}
