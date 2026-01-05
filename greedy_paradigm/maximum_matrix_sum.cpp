efac37ac6a4620da499c95270a2e9546481e8576
Manish Kumar <mankan2105@gmail.com>
2026-01-05 09:54:21 +0530

    obseravation

---
+++ greedy_paradigm/maximum_matrix_sum.cpp
@@ -0,0 +1,32 @@
+/*
+
+https://leetcode.com/problems/maximum-matrix-sum/description/?envType=daily-question&envId=2026-01-05
+
+
+ concept : observation
+     1st attempt \
+        |-> failed to do without hint, too easy problem 
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define loop(i,a,b)     for(int i=(a); i<(b); i++)
+#define all(x)          (x).begin(), (x).end()
+
+void setupIO() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main(int argc, char const *argv[]) { setupIO();
+
+
+
+    return 0;
+}
