04a90f5f6ce7990bfb125bc773811e0fa01493f4
Manish Kumar <mankan2105@gmail.com>
2026-01-23 14:17:06 +0530

    added both 1.sorting+2 pointer 2.multiset + binary search soln

--- cp_practise/dances.cpp
+++ cp_practise/dances.cpp
@@ -1,63 +1,106 @@
+/*
+ https://codeforces.com/problemset/problem/1883/G1
+
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
-#define loop(i,a,b)     for(int i=(a); i<(b); i++)
-#define all(x)          (x).begin(), (x).end()
+#define loop(i,a,b) for (int i = (a); i < (b); i++)
+#define all(x) (x).begin(), (x).end()
 
 void setupIO() {
-    ios::sync_with_stdio(false); cin.tie(nullptr);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
 }
 
-int main(int argc, char const *argv[]) { setupIO();
+// approach 1 : binary search + sorted multiset solution
 
-     int t; cin >> t;
-    
-       while(t--){
-         int N, M; cin >> N >> M;
+void solve1() {
+    int N, M;
+    cin >> N >> M;
 
+    multiset<int> msa, msb;
+    msa.insert(1);
 
-         multiset<int>msa, msb;
-         msa.insert(1);
+    for (int i = 2; i <= N; i++) {
+        int x;
+        cin >> x;
+        msa.insert(x);
+    }
 
-         for( int i = 2; i <= N; i++){
-             int x; cin >> x;
-             msa.insert(x);
-         }
+    for (int i = 1; i <= N; i++) {
+        int x;
+        cin >> x;
+        msb.insert(x);
+    }
 
-        for( int i = 1; i <= N; i++){
-             int x; cin >> x;
-             msb.insert(x);
+    int cnt = 0;
+    for (int el : msa) {
+        auto it = msb.upper_bound(el);
+
+        if (it != msb.end()) {
+            msb.erase(it);
+            cnt++;
+        } else {
+            break;
         }
+    }
 
-         int cnt = 0;
+    cout << N - cnt << '\n';
+}
 
-        for( int el : msa ){
-        
-           auto it = msb.upper_bound( el );
+// approach 2 : sorting + two pointer solution
 
-           if( it != msb.end() ) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-             msb.erase( msb.find(*it));
-             cnt++;
-             continue;
-           }
+void solve() {
+    int N, M;
+    cin >> N >> M;
 
-             break;
+    vector<int> arr_a(N), arr_b(N);
+
+    arr_a[0] = 1;
+    for (int i = 1; i < N; i++) {
+        cin >> arr_a[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr_b[i];
+    }
+
+    sort(all(arr_a));
+    sort(all(arr_b));
+
+    int i = N - 1, j = N - 1;
+    int cnt = 0;
+
+    while (i >= 0 && j >= 0) {
+        if (arr_a[i] >= arr_b[j]) {
+            i--;
+        } else {
+            i--;
+            j--;
+            cnt++;
         }
-       
-      
+    }
 
-      cout << N - cnt <<'\n';
+    cout << N - cnt << '\n';
+}
 
-        }
+int main() {
+    setupIO();
 
+    int t;
+    cin >> t;
 
+    while (t--) {
+        solve();
+    }
 
     return 0;
 }
-
-
-// write two pointer solution also
